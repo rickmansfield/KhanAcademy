1959be633ca6c59980b7642c80119529804297da
Rick <rmansfield@realpeoplesratings.com>
2021-04-30 16:05:00 -0500

    Update Snoopy Animal Attack

--- Computer_Programming/1-Intro_to_JS_Drawings_and_Animation/1-8-Bonus-Resizing_with_Variables/Project_Animal_Attach/index.js
+++ Computer_Programming/1-Intro_to_JS_Drawings_and_Animation/1-8-Bonus-Resizing_with_Variables/Project_Animal_Attach/index.js
@@ -11,37 +11,54 @@ var eyeH = bodyH/8;
 var earW = bodyW/3;
 var earH = bodyH/1;
 
-noStroke();
+noStroke();//to start
 
-draw = function() {
+let draw = function() {
+    //To have Snoopes Follow mouse
+    bodyX = mouseX;
+    bodyY = mouseY;
     
+    //General set up
     background(207, 254, 255);
     fill(250, 249, 245);
     stroke(8, 1, 1);
-    ellipse(bodyX+67, bodyY+8, snoutW, 75); // snout
-    ellipse(bodyX, bodyY, bodyW, bodyH); // skull
-    noStroke();
-    ellipse(bodyX+32, bodyY+8, snoutW, 75); // invisible circle snout
     
+    // snout
+    ellipse(bodyX+67, bodyY+8, snoutW, 75);
+    
+    //skull
+    ellipse(bodyX, bodyY, bodyW, bodyH); 
+    
+    //invisible circle snout
+    noStroke();
+    ellipse(bodyX+32, bodyY+8, snoutW, 75); 
+    
+    // nose
     stroke(3, 0, 0);
     fill(13, 1, 1);
-    ellipse(bodyX+70, bodyY+8, noseW, noseH); // nose
+    ellipse(bodyX+70, bodyY+8, noseW, noseH); 
+    
+    //Potential Body for later
     // bezier(114, 238, 126, 259, 67, 389, 104, 194);
     
-    rect(bodyX-29, bodyY+52, bodyW*0.5254, bodyH*0.09615, 30);//collar
+    //collar
+    rect(bodyX-29, bodyY+52, bodyW*0.5254, bodyH*0.09615, 30);
     
+    //Eyes
     ellipse(bodyX+5, bodyY-7, eyeW, eyeH); // left eye
     ellipse(bodyX+27, bodyY+-13, eyeW, eyeH); // right eye
     
-    ellipse(bodyX-50, bodyY+37, earW, earH); // nose
+    // nose
+    ellipse(bodyX-50, bodyY+37, earW, earH); 
     strokeWeight(2);
     fill(250, 247, 247);
     
-    arc(bodyX+42, bodyY+11, bodyW*0.8389, bodyH*0.57, 91, 126);//Snoopy's Smile
+    //Snoope's Smile
+    arc(bodyX+42, bodyY+11, bodyW*0.8389, bodyH*0.57, 91, 126);
     
-    bodyX +=1;
-    bodyY -=1;
-    
+    //When not following mouse use this code to self animate. 
+    // bodyX +=1;
+    // bodyY -=1;
     if (bodyX === 400) {
         bodyX = 20;
         bodyY = 300;
