1857648a23bcc04eb8a1bf825f4bc15313b1175a
Rick <rmansfield@realpeoplesratings.com>
2021-04-30 14:49:31 -0500

    Add My Favorite Foods Challenge

---
+++ Computer_Programming/1-Intro_to_JS_Drawings_and_Animation/1-9-Text_and_Strings/1-9-1_Challenge-My_Favorite_Foods/index.js
@@ -0,0 +1,9 @@
+fill(19, 74, 214);
+textSize(20);
+text("My Favorite Foods:", 50, 50);
+textSize(10);
+text("1.broccoli", 50, 60);
+textSize(10);
+text("2.cheese", 50, 70);
+textSize(10);
+text("3.pizza", 50, 80);
